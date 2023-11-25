21dd16132531a25d3372dc8a4b06b41a565c1fe4
Lukianov Viktor <barkadaft@gmail.com>
2023-11-26 01:24:13 +0300

    add if else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "yana")
+{
+    Console.WriteLine("Ура!!! Яночка пришла!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
